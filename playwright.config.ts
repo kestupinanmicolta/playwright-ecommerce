fe312d465a506340ce4a0dd62ca89e46fa9b2fac
Karen Paola Estupinan Micolta <kestupinanmicolta@gmail.com>
2026-08-19 18:22:01 -0500

    fix: proper HTML report generation for each tool

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,18 +19,6 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
-    },
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 13'] },
-    },
   ],
   expect: {
     toHaveScreenshot: {
